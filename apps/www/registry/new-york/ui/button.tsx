ed9919bb7cdf5cf310a4d88310548790924cdf5e
HakanAhmet <hag4126@g.rit.edu>
2024-12-02 21:03:35 -0500

    components changed: badge button calender checkbox switch

--- apps/www/registry/new-york/ui/button.tsx
+++ apps/www/registry/new-york/ui/button.tsx
@@ -10,15 +10,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-tprimary text-tsecondary shadow hover:bg-tprimary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-tred text-tsilver shadow-sm hover:bg-tred/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-tprimary text-tsilver bg-transparent shadow-sm hover:bg-tprimary hover:text-tsecondary",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-tsecondary text-tsilver shadow-sm hover:bg-tsecondary/80",
+        ghost: "hover:bg-tprimary hover:text-tsecondary",
+        link: "text-tprimary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
