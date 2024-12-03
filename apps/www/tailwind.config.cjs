de8a66626b65c219850cfa2a1c9c128bd8d2ea1a
HakanAhmet <hag4126@g.rit.edu>
2024-12-02 20:52:33 -0500

    thyris-colors to twconfig + bugfix

--- apps/www/tailwind.config.cjs
+++ apps/www/tailwind.config.cjs
@@ -8,4 +8,21 @@ module.exports = {
     "content/**/*.mdx",
     "registry/**/*.{ts,tsx}",
   ],
+  theme: {
+    extend: {
+      ...baseConfig.theme?.extend,
+      colors: {
+        ...baseConfig.theme?.extend?.colors,
+        tprimary: '#F9B21A',
+        tsecondary: '#1D2F54',
+        tblack: '#222831',
+        tgrey: '#30363F',
+        tsilver: '#E4E0E0',
+        tred: '#c1554e',
+      },
+      borderColor: {
+        border: 'hsl(var(--border))', // Add this line
+      },
+    },
+  },
 }
