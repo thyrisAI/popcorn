1c74798d9caec9d01280ba81b93c54d1f5488284
Hakan Gul <hag4126@g.rit.edu>
2024-12-04 08:53:48 -0500

    17 new components (#2)

--- apps/www/registry/new-york/ui/navigation-menu.tsx
+++ apps/www/registry/new-york/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-tprimary/80 hover:text-tblack focus:bg-tprimary/80 focus:text-tblack focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-tprimary data-[state=open]:bg-tprimary data-[active]:text-tblack data-[state=open]:text-tblack"
 )
 
 const NavigationMenuTrigger = React.forwardRef<
